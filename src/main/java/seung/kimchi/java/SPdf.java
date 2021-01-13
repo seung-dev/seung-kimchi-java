a73c874aed7b654fff2fc89b6c2f466a1e6deb4c
seung <seung@bigtech.co.kr>
2021-01-13 11:11:27 +0900

    add SPdf.encrypt

---
+++ src/main/java/seung/kimchi/java/SPdf.java
@@ -0,0 +1,82 @@
+package seung.kimchi.java;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
+import org.apache.pdfbox.pdmodel.encryption.StandardProtectionPolicy;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SPdf {
+
+	public SPdf() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static byte[] encrypt(
+			File file
+			, String key
+			) {
+		byte[] pdf = null;
+		try {
+			pdf = encrypt(FileUtils.readFileToByteArray(file), key, true, true, true, true);
+		} catch (IOException e) {
+			log.error("Failed to encrypt pdf file.", e);
+		}
+		return pdf;
+	}
+	public static byte[] encryptPDF(
+			byte[] data
+			, String key
+			) {
+		return encrypt(data, key, true, true, true, true);
+	}
+	public static byte[] encrypt(
+			byte[] data
+			, String key
+			, boolean allowPrinting
+			, boolean allowExtraction
+			, boolean allowModifications
+			, boolean readOnly
+			) {
+		
+		byte[] pdf = null;
+		
+		try(
+				PDDocument pdDocument = PDDocument.load(data);
+				ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+				) {
+			
+			AccessPermission accessPermission = new AccessPermission();
+			accessPermission.setCanExtractContent(allowExtraction);
+			accessPermission.setCanModify(allowModifications);
+			accessPermission.setCanPrint(allowPrinting);
+			if(readOnly) {
+				accessPermission.setReadOnly();
+			}
+			
+			StandardProtectionPolicy policy = new StandardProtectionPolicy(
+					key
+					, key
+					, accessPermission
+					);
+			policy.setEncryptionKeyLength(256);
+			
+			pdDocument.protect(policy);
+			pdDocument.save(byteArrayOutputStream);
+			
+			pdf = byteArrayOutputStream.toByteArray();
+			
+		} catch (IOException e) {
+			log.error("Failed to set key to pdf file.", e);
+		}
+		
+		return pdf;
+	}
+	
+}
