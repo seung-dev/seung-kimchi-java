98730ba5412ced11c8ccf3a0c9f21d027e7385f0
seung <seung@bigtech.co.kr>
2021-02-12 00:44:49 +0900

    add SDate.add

--- src/main/java/seung/kimchi/java/SDate.java
+++ src/main/java/seung/kimchi/java/SDate.java
@@ -2,12 +2,60 @@ package seung.kimchi.java;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
+
+import org.apache.commons.lang3.time.DateUtils;
 
 public class SDate {
 
+	public static final String _TIMEUNIT_MILLISECOND = "ms";
+	public static final String _TIMEUNIT_SECOND = "s";
+	public static final String _TIMEUNIT_MINUTE = "m";
+	public static final String _TIMEUNIT_HOUR = "h";
+	public static final String _TIMEUNIT_WEEK = "w";
+	public static final String _TIMEUNIT_DAY = "day";
+	public static final String _TIMEUNIT_MONTH = "month";
+	public static final String _TIMEUNIT_YEAR = "year";
+	
 	public SDate() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public static Date add(Date date, String timeUnit, int amount) {
+		
+		Date addedDate = null;
+		
+		switch (timeUnit) {
+			case _TIMEUNIT_MILLISECOND:
+				addedDate = DateUtils.addMilliseconds(date, amount);
+				break;
+			case _TIMEUNIT_SECOND:
+				addedDate = DateUtils.addSeconds(date, amount);
+				break;
+			case _TIMEUNIT_MINUTE:
+				addedDate = DateUtils.addMinutes(date, amount);
+				break;
+			case _TIMEUNIT_HOUR:
+				addedDate = DateUtils.addHours(date, amount);
+				break;
+			case _TIMEUNIT_WEEK:
+				addedDate = DateUtils.addWeeks(date, amount);
+				break;
+			case _TIMEUNIT_DAY:
+				addedDate = DateUtils.addDays(date, amount);
+				break;
+			case _TIMEUNIT_MONTH:
+				addedDate = DateUtils.addMonths(date, amount);
+				break;
+			case _TIMEUNIT_YEAR:
+				addedDate = DateUtils.addYears(date, amount);
+				break;
+			default:
+				break;
+		}
+		
+		return addedDate;
 	}
 	
 	public static int toInteger() {
@@ -41,6 +89,10 @@ public class SDate {
 	public static String toText(String pattern, Date date, TimeZone timeZone) {
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
 		simpleDateFormat.setTimeZone(timeZone);
+		return simpleDateFormat.format(date);
+	}
+	public static String toText(String pattern, Date date, Locale locale) {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern, locale);
 		return simpleDateFormat.format(date);
 	}
 	
