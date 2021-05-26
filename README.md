ed8994ec427f8efc2bb5863cb03aa6b2350ab9c2
seung@bigtech.co.kr <seung@bigtech.co.kr>
2021-05-26 16:11:16 +0900

    add jitpack configuration

--- README.md
+++ README.md
@@ -4,19 +4,38 @@
 
 - settings.gradle
 
-    ```
-    rootProject.name = "my-project-name"
-    
-    include "seung-kimchi-java"
-    project(":seung-kimchi-java").projectDir = new File("my-project-path/seung-kimchi-java")
-    ```
+```
+rootProject.name = "my-project-name"
+
+include "seung-kimchi-java"
+project(":seung-kimchi-java").projectDir = new File("my-project-path/seung-kimchi-java")
+```
 
 - gradle.build
 
-    ```
-    ...
-    implementation(project(path: ":seung-kimchi-java", configuration: "default")) {
-        exclude module: "slf4j-simple"
-    }
-    ...
-    ```
+```
+...
+implementation(project(path: ":seung-kimchi-java", configuration: "default")) {
+    exclude module: "slf4j-simple"
+}
+...
+```
+
+- jitpack gradle.build
+```
+...
+repositories {
+	mavenCentral()
+	maven { url "https://jitpack.io" }
+}
+...
+dependencies {
+	...
+	// seung
+	implementation("com.github.seung-dev:seung-kimchi-java:1.0.11") {
+		exclude module: "slf4j-simple"
+	}
+	...
+}
+...
+```
--- src/main/java/seung/kimchi/java/SXlsx.java
+++ src/main/java/seung/kimchi/java/SXlsx.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.Row;
@@ -20,6 +21,38 @@ import seung.kimchi.java.utils.excel.SSheet;
 @Slf4j
 public class SXlsx {
 
+	public static String[][] read(byte[] file, int sheetNo, int rowNoMax, int cellNoMax) {
+		
+		String[][] data = null;
+		
+		try {
+			
+			XSSFWorkbook workbook = new XSSFWorkbook(new ByteArrayInputStream(file));
+			Sheet sheet = workbook.getSheetAt(sheetNo);
+			Row row = null;
+			Cell cell = null;
+			
+			int getPhysicalNumberOfRows = sheet.getPhysicalNumberOfRows();
+			if(rowNoMax > getPhysicalNumberOfRows + 1) {
+				rowNoMax = getPhysicalNumberOfRows + 1;
+			}
+			
+			data = new String[getPhysicalNumberOfRows][cellNoMax];
+			for(int rowNo = 0; rowNo <= rowNoMax; rowNo++) {
+				row = sheet.getRow(rowNo);
+				for(int cellNo = 0; cellNo <= cellNoMax; cellNo++) {
+					cell = row.getCell(cellNo);
+					data[rowNo][cellNo] = cellValue(cell);
+				}
+			}
+			
+		} catch (Exception e) {
+			log.error("exception=", e);
+		}
+		
+		return data;
+	}
+	
 	public static SExcel read(byte[] file) {
 		
 		SExcel sExcel = SExcel.builder()
@@ -44,6 +77,7 @@ public class SXlsx {
 			
 		} catch (IOException e) {
 			sExcel.exception(e);
+			log.error("exception=", e);
 		}
 		
 		return sExcel;
@@ -77,25 +111,58 @@ public class SXlsx {
 				.rowIndex(cell.getRowIndex())
 				.columnIndex(cell.getColumnIndex())
 				.build();
-		if(CellType._NONE == cell.getCellTypeEnum()) {
-			sCell.setCellValue(null);
-		} else if(CellType.BLANK == cell.getCellTypeEnum()) {
-			sCell.setCellValue("" + cell.getBooleanCellValue());
-		} else if(CellType.BOOLEAN == cell.getCellTypeEnum()) {
-			sCell.setCellValue("" + cell.getBooleanCellValue());
-		} else if(CellType.STRING == cell.getCellTypeEnum()) {
-			sCell.setCellValue(cell.getStringCellValue());
-		} else if(CellType.NUMERIC == cell.getCellTypeEnum()) {
-			if(DateUtil.isCellDateFormatted(cell)) {
-				sCell.setCellValue("" + cell.getDateCellValue());
-			} else {
-				sCell.setCellValue(NumberToTextConverter.toText(cell.getNumericCellValue()));
-			}
-		} else if(CellType.FORMULA == cell.getCellTypeEnum()) {
-			sCell.setCellValue(cell.getCellFormula());
-		} else {
-			sCell.setCellValue("Check cell type.");
-		}
+		sCell.setCellValue(cellValue(cell));
 		return sCell;
 	}
+	
+	public static CellStyle[][] cellStyle(Sheet sheet, int rowNoMax, int cellNoMax) {
+		CellStyle[][] cellStyle = new CellStyle[rowNoMax + 1][cellNoMax + 1];
+		for(int rowNo = 0; rowNo <= rowNoMax; rowNo++) {
+			for(int cellNo = 0; cellNo <= cellNoMax; cellNo++) {
+				cellStyle[rowNo][cellNo] = sheet.getRow(rowNo).getCell(cellNo).getCellStyle();
+			}
+		}
+		return cellStyle;
+	}
+	
+	public static short[] rowHeight(Sheet sheet, int rowNoMax) {
+		short[] rowHeight = new short[rowNoMax + 1];
+		for(int rowNo = 0; rowNo <= rowNoMax; rowNo++) {
+			rowHeight[rowNo] = sheet.getRow(rowNo).getHeight();
+		}
+		return rowHeight;
+	}
+	
+	public static int[] columnWidth(Sheet sheet, int cellNoMax) {
+		int[] columnWidth = new int[cellNoMax + 1];
+		for(int cellNo = 0; cellNo <= cellNoMax; cellNo++) {
+			columnWidth[cellNo] = sheet.getColumnWidth(cellNo);
+		}
+		return columnWidth;
+	}
+	
+	public static String cellValue(Cell cell) {
+		
+		String cellValue = null;
+		if(CellType.STRING == cell.getCellTypeEnum()) {
+			cellValue = cell.getStringCellValue();
+		} else if(CellType.NUMERIC == cell.getCellTypeEnum()) {
+			cellValue = cell.getStringCellValue();
+		} else if(CellType.BOOLEAN == cell.getCellTypeEnum()) {
+			if(DateUtil.isCellDateFormatted(cell)) {
+				cellValue = "" + cell.getDateCellValue();
+			} else {
+				cellValue = NumberToTextConverter.toText(cell.getNumericCellValue());
+			}
+		} else if(CellType.FORMULA == cell.getCellTypeEnum()) {
+			cellValue = cell.getCellFormula();
+		} else if(CellType.BLANK == cell.getCellTypeEnum()) {
+			cellValue = "";
+		} else if(CellType._NONE == cell.getCellTypeEnum()) {
+			cellValue = "";
+		}
+		
+		return cellValue;
+	}
+	
 }
