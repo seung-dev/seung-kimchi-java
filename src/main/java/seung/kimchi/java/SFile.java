fabd71e0f0d3059e3b49b33146cfb932a385f1d9
DESKTOP-1Q5Q12B\BTP0004 <seung@bigtech.co.kr>
2021-12-01 15:03:01 +0900

    edit SFile.zip

--- src/main/java/seung/kimchi/java/SFile.java
+++ src/main/java/seung/kimchi/java/SFile.java
@@ -7,16 +7,14 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
-import org.apache.commons.io.FileUtils;
-
 import lombok.extern.slf4j.Slf4j;
+import seung.kimchi.java.utils.SLinkedHashMap;
 
 @Slf4j
 public class SFile {
@@ -44,6 +42,69 @@ public class SFile {
 		}
 		
 		return result;
+	}
+	
+	public static void add_zip_entry(
+			ZipOutputStream zipOutputStream
+			, String entry_path
+			) throws IOException {
+		
+		File file = new File(entry_path);
+		String fileName = file.getName();
+		
+		FileInputStream fileInputStream = null;
+		ZipEntry zipEntry = null;
+		try {
+			
+			fileInputStream = new FileInputStream(file);
+			zipEntry = new ZipEntry(fileName);
+			zipOutputStream.putNextEntry(zipEntry);
+			
+			byte[] b = new byte[1024 * 4];
+			int off = 0;
+			int len = 0;
+			while((len = fileInputStream.read(b)) >= 0) {
+				zipOutputStream.write(b, off, len);
+			}
+			
+			zipOutputStream.closeEntry();
+			
+		} catch (FileNotFoundException e) {
+			log.error("exception=", e);
+			throw e;
+		} catch (IOException e) {
+			throw e;
+		} finally {
+			try {
+				if(fileInputStream != null) {
+					fileInputStream.close();
+				}
+			} catch (IOException e) {
+				log.error("exception=", e);
+			}
+		}
+		
+	}
+	
+	public static int isZip(String filePath) {
+		int isZip = 0;
+		ZipFile zipFile = null;
+		try {
+			zipFile = new ZipFile(filePath);
+			isZip = zipFile.size();
+		} catch (Exception e) {
+			log.error("exception=", e);
+			isZip = -1;
+		} finally {
+			try {
+				if(zipFile != null) {
+					zipFile.close();
+				}
+			} catch (IOException e) {
+				log.error("exception=", e);
+			}
+		}
+		return isZip;
 	}
 	
 	public static int zip(
@@ -87,79 +148,20 @@ public class SFile {
 		return isZip(file_path);
 	}
 	
-	public static int isZip(String filePath) {
-		int isZip = 0;
-		ZipFile zipFile = null;
-		try {
-			zipFile = new ZipFile(filePath);
-			isZip = zipFile.size();
-		} catch (Exception e) {
-			log.error("exception=", e);
-			isZip = -1;
-		} finally {
-			try {
-				if(zipFile != null) {
-					zipFile.close();
-				}
-			} catch (IOException e) {
-				log.error("exception=", e);
-			}
-		}
-		return isZip;
-	}
-	
-	public static byte[] zip(
-			List<String> file_path_list
-			) {
-		
-		byte[] zip = null;
-		
-		ByteArrayOutputStream byteArrayOutputStream = null;
-		ZipOutputStream zipOutputStream = null;
-		try {
-			
-			byteArrayOutputStream = new ByteArrayOutputStream();
-			zipOutputStream = new ZipOutputStream(byteArrayOutputStream);
-			
-			for(String entry_path : file_path_list) {
-				add_zip_entry(zipOutputStream, entry_path);
-			}
-			
-			zipOutputStream.flush();
-			
-		} catch (Exception e) {
-			log.error("exception=", e);
-		} finally {
-			try {
-				if(zipOutputStream != null) {
-					zipOutputStream.close();
-				}
-				if(byteArrayOutputStream != null) {
-					zip = byteArrayOutputStream.toByteArray();
-					byteArrayOutputStream.close();
-				}
-			} catch (IOException e) {
-				log.error("exception=", e);
-			}
-		}
-		
-		return zip;
-	}
-	
 	public static void add_zip_entry(
 			ZipOutputStream zipOutputStream
 			, String entry_path
+			, String entry_name
 			) throws IOException {
 		
 		File file = new File(entry_path);
-		String fileName = file.getName();
 		
 		FileInputStream fileInputStream = null;
 		ZipEntry zipEntry = null;
 		try {
 			
 			fileInputStream = new FileInputStream(file);
-			zipEntry = new ZipEntry(fileName);
+			zipEntry = new ZipEntry(entry_name);
 			zipOutputStream.putNextEntry(zipEntry);
 			
 			byte[] b = new byte[1024 * 4];
@@ -186,6 +188,44 @@ public class SFile {
 			}
 		}
 		
+	}
+	
+	public static byte[] zip(
+			List<SLinkedHashMap> file_list
+			) {
+		
+		byte[] zip = null;
+		
+		ByteArrayOutputStream byteArrayOutputStream = null;
+		ZipOutputStream zipOutputStream = null;
+		try {
+			
+			byteArrayOutputStream = new ByteArrayOutputStream();
+			zipOutputStream = new ZipOutputStream(byteArrayOutputStream);
+			
+			for(SLinkedHashMap entry_file : file_list) {
+				add_zip_entry(zipOutputStream, entry_file.getString("file_path"), entry_file.getString("file_name"));
+			}
+			
+			zipOutputStream.flush();
+			
+		} catch (Exception e) {
+			log.error("exception=", e);
+		} finally {
+			try {
+				if(zipOutputStream != null) {
+					zipOutputStream.close();
+				}
+				if(byteArrayOutputStream != null) {
+					zip = byteArrayOutputStream.toByteArray();
+					byteArrayOutputStream.close();
+				}
+			} catch (IOException e) {
+				log.error("exception=", e);
+			}
+		}
+		
+		return zip;
 	}
 	
 }
